78d6de4707e5f323243f4b2a8581016ddfa0b919
011000101101 <011000101101@gmx.de>
2020-07-06 23:23:34 +0200

    added some pickle dumps and added documentation to precompute/create_kanji_image_samples.py

--- precompute/create_kanji_image_samples.py
+++ precompute/create_kanji_image_samples.py
@@ -5,13 +5,12 @@ from PIL import ImageFont, ImageDraw, Image
 import cv2
 import pickle
 
-with open("../bin_blobs/kanji_list.pkl", 'rb') as f:
-    kanji_list = pickle.load(f)
-
-with open("../bin_blobs/faulty_kanji.pkl", 'rb') as f:
-    faulty_img = pickle.load(f)
-
+# specify the font size in pt, this has to be in relation with image size in pixels
 FONT_SIZE_PT = 48
+# specify image pixel count (horizontal and vertical, square image)
+IMAGE_SIZE_PX = 50
+
+# specify names and paths of available fonts
 font_paths = [
     ("komorebi_gothic", "MODI_komorebi-gothic_2018_0501/komorebi-gothic.ttf"),
     ("kosugi_maru", "Kosugi_Maru/KosugiMaru-Regular.ttf"),
@@ -26,46 +25,79 @@ font_paths = [
     ("nikkyou_sans", "NikkyouSans/NikkyouSans-B6aV.ttf"),
     ("xano_mincho", "Xano-Mincho/XANO-mincho-U32.ttf")
 ]
+
+# specify colour for font rendering
 b, g, r, a = 0, 0, 0, 0  # totally black
-empty_img = np.ones((50,50,3),np.uint8)
+
+# specify empty white image, which is computed once and then copied multiple times for performance reasons
+empty_img = np.ones((IMAGE_SIZE_PX,IMAGE_SIZE_PX,3),np.uint8)
 empty_img *= 255  # pure white background
 
 
 def create_samples():
+    """
+    create image samples for each kanji: first load list of kanji from disk, then render each kanji using different
+    fonts, finally save image samples to disk
+    :return:
+    """
 
+    # load a list of all kanji to be rendered from the disk
+    with open("../bin_blobs/kanji_list.pkl", 'rb') as f:
+        kanji_list = pickle.load(f)
+
+    # load an image of a faulty rendering to filter out unsupported kanji-font-combinations from disk
+    # kanji unsupported by a certain font usually don't render at all, leaving an empty image, but sometimes they render
+    # as a specific rectangle, identical to the image loaded here
+    with open("../resources/fonts/kanji_image_faulty_rendering.pkl", 'rb') as f:
+        faulty_img = pickle.load(f)
+
+    # initialize dictionary to hold image samples indexed by kanji
     image_samples = dict()
 
+    # load all available fonts
     fonts = []
     for font_path in font_paths:
+        # complete font path
         font_path_absolute = "../resources/fonts/{}".format(font_path[1])
+        # load font
         font = ImageFont.truetype(font_path_absolute, FONT_SIZE_PT)
         fonts.append((font_path[0], font))
 
+    # render each kanji...
     for kanji in kanji_list:
 
-        try:
-            image_samples[kanji]
-        except KeyError:
-            image_samples_tmp = []
+        image_samples_tmp = []
 
-            for font in fonts:
+        # ...with each font
+        for font in fonts:
 
-                img_tmp = empty_img.copy()
-                img_pil = Image.fromarray(img_tmp)
-                draw = ImageDraw.Draw(img_pil)
-                draw.text((0, 0), kanji, font=font[1], fill=(b, g, r, a))
-                img = np.array(img_pil)
+            # create empty white image
+            img_tmp = empty_img.copy()
+            # convert to correct format
+            img_pil = Image.fromarray(img_tmp)
+            # render single kanji by putting text onto the image
+            draw = ImageDraw.Draw(img_pil)
+            draw.text((0, 0), kanji, font=font[1], fill=(b, g, r, a))
+            # convert image back to numpy array
+            img = np.array(img_pil)
 
-                if (img == empty_img).all() or (img == faulty_img).all():
-                    # print("character not supported: {}, font {}".format(kanji, font[0]))
-                    pass
-                else:
-                    # cv2.imshow("asdf", img);cv2.waitKey();cv2.destroyAllWindows()
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    image_samples_tmp.append((font[0], img))
+            # filter out not or faultily rendered kanji-font-combinations
+            if (img == empty_img).all() or (img == faulty_img).all():
+                # print("character not supported: {}, font {}".format(kanji, font[0]))
+                pass
+            else:
+                # cv2.imshow("asdf", img);cv2.waitKey();cv2.destroyAllWindows()
+                # convert image to greyscale to save space (semantically it is already in greyscale anyways, so just
+                # use different encodiing)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            image_samples[kanji] = image_samples_tmp
+                # accumulate rendered images
+                image_samples_tmp.append((font[0], img))
 
+        # insert all images for a certain kanji into the dict
+        image_samples[kanji] = image_samples_tmp
+
+    # save on disk
     with open("../bin_blobs/kanji_image_samples.pkl", 'wb') as f:
         pickle.dump(image_samples, f, pickle.HIGHEST_PROTOCOL)
 
