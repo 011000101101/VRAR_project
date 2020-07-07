c72059f02337f32ab9aba1e878ef26fb9d860d16
011000101101 <011000101101@gmx.de>
2020-07-07 16:58:19 +0200

    fixed naming conflict in ./precompute/

--- precompute/precompute_all.py
+++ precompute/precompute_all.py
@@ -3,7 +3,8 @@ import precompute.convert_kanjidic2_to_hashtable as convert_kanjidic2_to_hashtab
 import precompute.convert_kanjidic2_to_list as convert_kanjidic2_to_list
 import precompute.create_kanji_image_samples as create_kanji_image_samples
 
-convert_JMdict_to_hashtable.convert()
-convert_kanjidic2_to_hashtable.convert()
-convert_kanjidic2_to_list.convert()
-create_kanji_image_samples.create_samples()
+if __name__ == "__main__":
+    convert_JMdict_to_hashtable.convert()
+    convert_kanjidic2_to_hashtable.convert()
+    convert_kanjidic2_to_list.convert()
+    create_kanji_image_samples.create_samples()
